067286b28437dbef20052a401edaf434ec83e639
Phoon Seng Wei <phoonsengwei321@gmail.com>
2021-08-02 14:14:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/activitylifecycle_demo/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.android.activitylifecycle_demo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn : Button = findViewById(R.id.btnNext)
+
+        btn.setOnClickListener(){
+            var intent : Intent = Intent(this, SecondActivity::class.java )
+
+            startActivity(intent)
+        }
+    }
+
+}
